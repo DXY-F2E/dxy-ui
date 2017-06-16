734d6bfe6ea17cf5621a4467459022f84fb96d2f
liww <liww@lwenndeMacBook-Pro.local>
2017-06-16 17:58:26 +0800

    项目目录结构和插件引入

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/**
+ * Created by liww on 17/6/15.
+ */
+
+// 引入组件
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+
+gulp.task('css', function() {
+  return gulp
+    .src('sass/dxy-ui.scss')
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(postcss([
+      require('autoprefixer')
+    ]))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('cssmin', ['css'], function() {
+  return gulp
+    .src('dist/css/dxy-ui.css')
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['sass/**/*.scss', 'sass/mixins/**/*.scss'], ['cssmin']);
+});
+
+gulp.task('default', ['css', 'cssmin']);
