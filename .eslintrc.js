81c94248facba3462c1e8b56838a93f6d7ab3e92
uojo <friend1020@163.com>
2019-01-15 17:35:37 +0800

    feat 添加 eslint 配置

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    "ecmaVersion": 6
+  },
+  env: {
+    node: true
+  },
+  globals:{
+  },
+  extends: ['standard'],
+  plugins: [
+    
+  ],
+  rules: {
+
+  }
+}
