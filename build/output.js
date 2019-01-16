f2552c5551dce97d5b63b4988cc0dab5c5acfff6
uojo <friend1020@163.com>
2019-01-16 18:32:13 +0800

    压缩输出的 html 文件

--- build/output.js
+++ build/output.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 const tool = require('./tool');
+const { minify } = require('html-minifier');
 
-const docsHtml = tool.reanderPageDocs();
+const minifyFn = html => minify(html, {
+  collapseWhitespace: true,
+  collapseInlineTagWhitespace: true,
+});
+
+const docsHtml = minifyFn(tool.reanderPageDocs());
 fs.writeFileSync(tool.resoveRoot('docs/index.html'), docsHtml, 'utf-8');
 
-const examplesHtml = tool.readerPageExamples();
+const examplesHtml = minifyFn(tool.readerPageExamples());
 fs.writeFileSync(tool.resoveRoot('examples/index.html'), examplesHtml, 'utf-8');
